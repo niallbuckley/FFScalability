b07404fbde6fa6c1a2cc1cf674b12ba0d377bfab
Niall Buckley <buckleyniall100@gmail.com>
2019-08-05 23:12:22 -0400

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,7 +1,7 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-it('Main page content', function(done) {
+it('Does body display hello world', function(done) {
     request('http://localhost:3000' , function(error, response, body) {
         expect(body).to.equal('Hello World!!!!');
         done();
